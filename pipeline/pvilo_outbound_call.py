19470d3ad60c6cdcb695418a6cd6a85e1d85c981
Vadim Astakhov <testpilot0@gmail.com>
2021-10-06 15:11:25 -0700

    Update pvilo_outbound_call.py

--- pipeline/pvilo_outbound_call.py
+++ pipeline/pvilo_outbound_call.py
@@ -1,1 +1,8 @@
-
+import plivo
+auth_id = ‘Your AUTH ID’
+auth_token = ‘Your AUTH Token’
+phlo_id = ‘Your PHLO ID’ # https://console.plivo.com/phlo/list/
+phlo_client = plivo.phlo.RestClient(auth_id=auth_id, auth_token=auth_token)
+phlo = phlo_client.phlo.get(phlo_id)
+response = phlo.run()
+print str(response)
